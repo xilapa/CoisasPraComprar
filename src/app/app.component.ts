980abfde905a0fa6dfe41b782cd311cebdf65834
dirceu junior <dirceu.sjr@gmail.com>
2021-03-26 00:04:35 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cpc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CoisasPraComprar';
+}
